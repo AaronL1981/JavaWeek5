0d2dbcc4fb789a18d054e1c9badc103ed1c07cf5
AaronL1981 <104656550+AaronL1981@users.noreply.github.com>
2022-06-05 15:11:50 -0700

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,14 @@
+/**
+ * 1. Create an interface named Logger.
+ * 2. Add two void methods to the Logger interface, each should take a String as an arugment
+ *          a. Log
+ *          b. Error
+ * @author aaronlindsay
+ *
+ */
+public interface Logger {
+  public void Log(String str);
+  public void Error(String str);
+ 
+
+}
